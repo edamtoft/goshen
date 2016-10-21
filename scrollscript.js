baf09f32cf719d8a00a8a828022f2385d239b6eb
Eric Damtoft <edamtoft@gmail.com>
2016-10-21 14:51:23 -0400

    I did a lot of stuff [Moses]
    I also decided I should start labeling which changes I made, because
    it's all under Eric's account.
    
    Things accomplished:
    Switched to flexbox with the MB lists on the camp pages.
    Made a specialty week page.
    Made the navbar stick when needed.
    Dealt with the overlap vertical responsiveness by adding min-heights to
    the home page banner.
    I probably made the footer in this edit, too, which means I didn't push
    before I took a break to go figure out how to design emails (ie, the
    land of tables). I missed divs terribly.

---
+++ scrollscript.js
@@ -0,0 +1,28 @@
+$(function() {
+  //debouncing function so that functions don't fire too much and brick the page
+  /*function debounce(func, wait, immediate) {
+    var timeout;
+    return function() {
+      var context = this, args = arguments;
+      var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  };
+*/
+  //for making the navbar do the cool scroll-then-stick thing
+    $(window).scroll(function(){
+      console.log($(window).scrollTop());
+      if ($(window).scrollTop()>40) {
+          $(".gsr-navbar").addClass("gsr-fixed-nav");
+      }
+      if ($(window).scrollTop()<41) {
+          $(".gsr-navbar").removeClass("gsr-fixed-nav");
+      }
+    });
+});
